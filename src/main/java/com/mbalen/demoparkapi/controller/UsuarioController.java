4b29a719482251987c105563aa781b09cc7e9012
Tuyzera <utpadelarthur@gmail.com>
2024-02-02 12:49:02 -0300

    Create a GET and POST API for the user entity

--- src/main/java/com/mbalen/demoparkapi/controller/UsuarioController.java
+++ src/main/java/com/mbalen/demoparkapi/controller/UsuarioController.java
@@ -1,13 +1,29 @@
 package com.mbalen.demoparkapi.controller;
 
+import com.mbalen.demoparkapi.entity.Usuario;
 import com.mbalen.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/usuarios")
+@RequestMapping("api/v1/usuarios") //Url de acesso
 public class UsuarioController {
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user =  usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUserById(@PathVariable long id){
+        Usuario user = usuarioService.buscarPorID(id);
+        return ResponseEntity.ok(user);
+
+    }
+
 }
--- src/main/java/com/mbalen/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/mbalen/demoparkapi/service/UsuarioService.java
@@ -1,12 +1,27 @@
 package com.mbalen.demoparkapi.service;
 
+import com.mbalen.demoparkapi.entity.Usuario;
 import com.mbalen.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RequiredArgsConstructor //Criar um metodo construtor com a variavel usuario repository
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+    @Transactional(readOnly = true) //Exclusivo para uma consulta ao banco de dados
+    public Usuario buscarPorID(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado!")
+        );
+    }
 }
